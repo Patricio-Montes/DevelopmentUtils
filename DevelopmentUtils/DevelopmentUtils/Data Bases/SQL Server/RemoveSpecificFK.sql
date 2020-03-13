2e3e9fa345815210d95ea5583f5697882bf27536
Patricio Montes <patricio.montes@codeicus.com>
2020-03-13 10:05:19 -0300

    Added: New SQL util and WorkDB Kee pass.

---
+++ DevelopmentUtils/DevelopmentUtils/Data Bases/SQL Server/RemoveSpecificFK.sql
@@ -0,0 +1,13 @@
+SELECT * 
+FROM sys.foreign_keys
+WHERE referenced_object_id = object_id('notification_core_request')
+
+
+SELECT 
+    'ALTER TABLE [' +  OBJECT_SCHEMA_NAME(parent_object_id) +
+    '].[' + OBJECT_NAME(parent_object_id) + 
+    '] DROP CONSTRAINT [' + name + ']'
+FROM sys.foreign_keys
+WHERE referenced_object_id = object_id('notification_core_request')
+
+
