45a0bc5fb7da69925410b2ff25eff06b74a7f1e0
Patricio Montes <patricio.montes@codeicus.com>
2020-04-21 14:34:00 -0300

    Commit

---
+++ DevelopmentUtils/DevelopmentUtils/BackEnd/Java/Data Types/DateTime.java
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevelopmentUtils.BackEnd.Java.Data_Types
+{
+    class DateTime
+    {
+		// const string DEFAULT_TIME_FORMAT = HH:mm:ss
+		String MAX_TIME_VALUE = LocalTime.parse(LocalTime.MAX.toString(), DateTimeFormatter.ofPattern(DEFAULT_TIME_FORMAT)).toString()
+    }
+}
--- DevelopmentUtils/DevelopmentUtils/Data Bases/SQL Server/LinkedServersInteractions.sql
+++ DevelopmentUtils/DevelopmentUtils/Data Bases/SQL Server/LinkedServersInteractions.sql
@@ -1,1 +1,4 @@
-select * from [ServerName/ServerIP].comdb.dbo.TableName
+select * from [ServerName/ServerIP].DBName.dbo.TableName
+
+update [ServerName/ServerIP].DBName.dbo.TableName set Image_Column = (SELECT BulkColumn 
+FROM Openrowset( Bulk 'C:\cbimage.jpg', Single_Blob) as image)
